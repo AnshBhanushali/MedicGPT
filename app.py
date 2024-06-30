f546a2dccf711edfa82a021f3411e0acac861818
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-06-30 01:11:45 -0400

    added elements in git ignore

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import os
 from flask import Flask, render_template, request
 import openai
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+openai.api_key = os.getenv('API_KEY')
 
