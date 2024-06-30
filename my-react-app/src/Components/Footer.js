cb568436e19b6d57e55ca0f9563dacc85db8bc22
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-06-30 03:45:12 -0400

    created footer component

---
+++ my-react-app/src/Components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Footer = () => {
+  return (
+    <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+      <Typography variant="h6" align="center" gutterBottom>
+        Medic App
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        align="center"
+        color="text.secondary"
+        component="p"
+      >
+        Your health, our priority.
+      </Typography>
+    </Box>
+  );
+}
+
+export default Footer;
