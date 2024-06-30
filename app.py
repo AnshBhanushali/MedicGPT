910b606222c7a44bf9c5b4028e62a7c2143e00b4
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-06-30 02:22:51 -0400

    created dependencies for react + tailwind css

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
 import os
 from flask import Flask, render_template, request
-import openai
+from openai import OpenAI
+client = OpenAI(api_key=os.getenv('API_KEY'))
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 
-openai.api_key = os.getenv('API_KEY')
-
 
 @app.route("/", methods=["GET", "POST"])
 def chatbot():
+    user_input = None
+    chat_response = None
     if request.method == "POST":
         user_input = request.form.get("user_input")
-        chat_history = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+        response = client.chat.completions.create(model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "You are a helpful medical chatbot."},
                 {"role": "user", "content": user_input}
-            ]    
-        )
-
-        chat_response = chat_history["choices"][0]["message"]["content"]
-        return render_template("index.html", user_input=user_input, chat_response=chat_response)
+            ])
     
-    return render_template("index.html")
+        chat_response = response.choices[0].message.content
+
+    return render_template("index.html", user_input=user_input, chat_response=chat_response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=True, port=port)
